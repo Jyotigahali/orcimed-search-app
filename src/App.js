8aa3a99a46ff33e00dd57473afab0539973f0775
Jyori-Ghali <jyoti.ghali@orcimedlifesciences.com>
2024-09-29 19:56:02 +0530

    test commit1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button'
 
 function App() {
   return (
     <div className="App">
      <h1>
       Cipla Search Tool
-      
      </h1>
+     <Button>Test Search toll</Button>
     </div>
   );
 }
